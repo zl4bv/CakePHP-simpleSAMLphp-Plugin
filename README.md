776044c0fb6cec4b0106cb4b3568f1e9ac88d602
Ben Vidulich <ben@vidulich.co.nz>
2012-12-02 22:23:15 +1300

    Initial import

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 CakePHP-simpleSAMLphp-Plugin
-============================
+==================
 
 Adds support for SAML authentication to CakePHP 2.x.
+
+Requirements
+------------
+
+* Latest version of simpleSAMLphp (>= 1.9.0)
+* Latest version of CakePHP (>= 2.2.3)
+
+Installation
+------------
+
+Simply drop the plugin folder in the `app/Plugin` folder.
+
+The plugin needs to be loaded manually in `app/Config/bootstrap.php`.
+
+You can either load the plugin in by name, or load all plugins with a single call:
+    CakePlugin::loadAll(); // Loads all plugins at once
+    CakePlugin::load('Saml'); //Loads a single plugin
+
+Configuration
+-------------
+
+Configuration is done in `app/Config/core.php`.
+
+Path to SimpleSAMLphp installation.
+    Configure::write('Saml.SimpleSamlPath', '/path/to/simpleSAMLphp');
+
+Authentication source to use. Defaults to 'default-sp'.
+    Configure::write('Saml.AuthSource', 'default-sp');
+    
+Usage
+-----
+
+Add this to your controller:
+    public $components = array('Saml.Saml');
+    
+You can now call the following to require a user to be logged in to view the current page:
+    $this->Saml->requireAuth();
+    
+Troubleshooting
+---------------
+
+Check the following logs for possible information:
+
+* CakePHP error log (default is `app/tmp/logs/error.log`)
+* Apache error log
+* PHP error log (if different from the Apache error log)
+* simpleSAMLphp log
---
+++ Saml/Controller/Component/SamlComponent.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Simple component to bind to the SimpleSAML_Auth_Simple class.
+ * @author Ben Vidulich
+ * @link http://simplesamlphp.org/docs/stable/simplesamlphp-sp-api
+ *
+ */
+class SamlComponent extends Component {
+	private $path = NULL;
+	private $authSource = 'default-sp';
+	private $as;
+
+	/**
+	 * Initializes a SimpleSAML_Auth_Simple object.
+	 *
+	 * @param string $authSource The ID of the authentication source to use. This will
+	 * override the authentication source set in `app/Config/bootstrap.php`.
+	 */
+	public function __construct(ComponentCollection $collection, array $settings, string $authSource = NULL) {
+		// Check the config
+		if (Configure::read('Saml.SimpleSamlPath') != NULL) {
+			$this->path = Configure::read('Saml.SimpleSamlPath');
+		} else {
+			throw new Exception('Parameter Saml.SimpleSamlPath is missing from the configuration file.');
+		}
+		
+		if ($authSource != NULL) {
+			$this->authSource = $authSource;
+		} elseif (Configure::read('Saml.AuthSource') != NULL) {
+			$this->authSource = Configure::read('Saml.AuthSource');
+		}
+		
+		// Initialize simpleSAMLphp
+		require_once($this->path.'/lib/_autoload.php');
+		$this->as = new SimpleSAML_Auth_Simple($this->authSource);
+	}
+
+	/**
+	 * Retrieve the attributes of the current user. If the user isn't authenticated, an empty array will be returned.
+	 */
+	public function getAttributes() {
+		return $this->as->getAttributes();
+	}
+	
+	/**
+	 * Retrieves the URL that can start the authentication process.
+	 * 
+	 * @param string $returnTo The URL the user will be returned to. By default it is the current page.
+	 */
+	public function getLoginURL(string $returnTo = NULL) {
+		return $this->as->getLoginURL($returnTo);	
+	}
+	
+	/**
+	 * Retrieves the URL that can trigger a logout.
+	 *
+	 * @param string $returnTo The URL the user will be returned to. By default it is the current page.
+	 */
+	public function getLogoutURL(string $returnTo = NULL) {
+		return $this->as->getLogoutURL($returnTo);
+	}
+
+	/**
+	 * Returns TRUE if the user is authenticated.
+	 */
+	public function isAuthenticated() {
+		return $this->as->isAuthenticated();
+	}
+
+	/**
+	 * Starts the authentication process.
+	 *
+	 * @param mixed $url The URL to redirect to after logging in, or an associative array of parameters.
+	 */
+	public function login(mixed $url) {
+		$this->as->login($url);
+	}
+
+	/**
+	 * Logs the user out.
+	 *
+	 * @param mixed $url The URL to redirect to after logging out, or an associative array of parameters.
+	 */
+	public function logout(mixed $url) {
+		$this->as->logout($url);
+	}
+
+	/**
+	 * Ensures the user is authenticated. Starts the authentication process if the user isn't authenticated.
+	 *
+	 * @param array $params Associative array with named parameters. See the simplePHPphp documentation for a description of the paramters.
+	 */
+	public function requireAuth(array $params = array()) {
+		$this->as->requireAuth($params);
+	}
+}
+?>
